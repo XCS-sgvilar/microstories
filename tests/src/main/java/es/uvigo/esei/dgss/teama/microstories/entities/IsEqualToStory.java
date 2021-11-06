10afe963f3942f2b22a36c828cf87f128eed6a78
Rodrigo Currás <rcferradas@esei.uvigo.es>
2021-11-06 12:43:06 +0100

    tsk 4 - Add Story matcher
    Add Story matcher for tests

---
+++ tests/src/main/java/es/uvigo/esei/dgss/teama/microstories/entities/IsEqualToStory.java
@@ -0,0 +1,55 @@
+package es.uvigo.esei.dgss.teama.microstories.entities;
+
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+
+
+public class IsEqualToStory extends IsEqualToEntity<Story> {
+
+    public IsEqualToStory(Story story) {
+        super(story);
+    }
+
+    @Override
+    protected boolean matchesSafely(Story actual) {
+        this.clearDescribeTo();
+
+        if (actual == null) {
+            this.addTemplatedDescription("actual", expected.toString());
+            return false;
+        } else {
+            return checkAttribute("title", Story::getTitle, actual)
+                    && checkAttribute("content", Story::getContent, actual)
+                    && checkAttribute("date", Story::getDate, actual)
+                    && checkAttribute("genre", Story::getGenre, actual)
+                    && checkAttribute("theme", Story::getTheme, actual)
+                    && checkAttribute("publicated", Story::getPublicated, actual);
+        }
+    }
+
+    @Factory
+    public static IsEqualToStory equalToStory(Story story) {
+        return new IsEqualToStory(story);
+    }
+
+    @Factory
+    public static Matcher<Iterable<? extends Story>> containsStorysInAnyOrder(Story ... stories) {
+        return containsEntityInAnyOrder(IsEqualToStory::equalToStory, stories);
+    }
+
+    @Factory
+    public static Matcher<Iterable<? extends Story>> containsStorysInAnyOrder(Iterable<Story> stories) {
+        return containsEntityInAnyOrder(IsEqualToStory::equalToStory, stories);
+    }
+
+    @Factory
+    public static Matcher<Iterable<? extends Story>> containsStorysInOrder(Story ... stories) {
+        return containsEntityInOrder(IsEqualToStory::equalToStory, stories);
+    }
+
+    @Factory
+    public static Matcher<Iterable<? extends Story>> containsStorysInOrder(Iterable<Story> stories) {
+        return containsEntityInOrder(IsEqualToStory::equalToStory, stories);
+    }
+
+}
