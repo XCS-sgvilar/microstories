6ffe3b326edfa179c4148a5d6f98583758f40f51
Santiago Gómez <santii810@hotmail.com>
2021-11-23 20:22:18 +0100

    tks 14 - Adds functionality to search stories
    Adds method to find stories filtering query by genre, theme, and date range

--- service/src/main/java/es/uvigo/esei/dgss/teamA/microstories/service/StoryService.java
+++ service/src/main/java/es/uvigo/esei/dgss/teamA/microstories/service/StoryService.java
@@ -1,12 +1,15 @@
 package es.uvigo.esei.dgss.teamA.microstories.service;
 
+import es.uvigo.esei.dgss.teamA.microstories.entities.Genre;
 import es.uvigo.esei.dgss.teamA.microstories.entities.Story;
+import es.uvigo.esei.dgss.teamA.microstories.entities.Theme;
 
 import javax.annotation.security.PermitAll;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -44,6 +47,23 @@ public class StoryService {
                                 "ORDER BY s.date DESC, s.id", Story.class)
                 .setFirstResult(getStartPagination(page, size)).setMaxResults(checkSize(size));
         query.setParameter(1, text);
+        return query.getResultList();
+    }
+
+    @PermitAll
+    public List<Story> findStories(final Genre genre, Theme theme, Date initDate, Date endDate, Integer page, Integer size) throws IllegalArgumentException {
+        final TypedQuery<Story> query = em.createQuery("SELECT s FROM Story s " +
+                        "WHERE (:genre is null OR s.genre = :genre) " +
+                        "and (:theme is null or s.mainTheme = :theme or s.secondaryTheme = :theme) " +
+                        "and (:initDate is null or s.date >= :initDate)" +
+                        "and (:endDate is null or s.date <= :endDate) " +
+                        "ORDER BY s.date DESC, s.id", Story.class)
+                .setFirstResult(getStartPagination(page, size))
+                .setMaxResults(checkSize(size));
+        query.setParameter("genre", genre);
+        query.setParameter("theme", theme);
+        query.setParameter("initDate", initDate);
+        query.setParameter("endDate", endDate);
         return query.getResultList();
     }
 
--- service/src/test/java/es/uvigo/esei/dgss/teamA/microstories/service/StoryServiceIntegrationTest.java
+++ service/src/test/java/es/uvigo/esei/dgss/teamA/microstories/service/StoryServiceIntegrationTest.java
@@ -1,8 +1,11 @@
 package es.uvigo.esei.dgss.teamA.microstories.service;
 
 
+import es.uvigo.esei.dgss.teamA.microstories.entities.Genre;
 import es.uvigo.esei.dgss.teamA.microstories.entities.Story;
 import es.uvigo.esei.dgss.teamA.microstories.entities.StoryDataset;
+import es.uvigo.esei.dgss.teamA.microstories.entities.Theme;
+import org.hamcrest.collection.IsEmptyCollection;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.persistence.CleanupUsingScript;
@@ -17,7 +20,10 @@ import org.junit.runner.RunWith;
 
 import javax.ejb.EJBTransactionRolledbackException;
 import javax.inject.Inject;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static es.uvigo.esei.dgss.teamA.microstories.entities.IsEqualToStory.containsStorysInOrder;
 import static es.uvigo.esei.dgss.teamA.microstories.entities.IsEqualToStory.equalToStory;
@@ -155,5 +161,136 @@ public class StoryServiceIntegrationTest {
     @Test(expected = EJBTransactionRolledbackException.class)
     public void testFindStoriesByTextNull() {
         List<Story> storyList = this.facade.findStoriesByText(null, 0, SIZE);
+    }
+
+
+    @Test
+    @ShouldMatchDataSet("stories.xml")
+    public void testFindStoriesWithoutFilters() {
+        List<Story> storyList = this.facade.findStories(null, null, null, null, null, null);
+
+        final List<Story> recentStories = recentStories().subList(0, SIZE);
+
+        Assert.assertNotNull(storyList);
+        Assert.assertThat(storyList, hasSize(SIZE));
+        assertThat(recentStories, containsStorysInOrder(storyList));
+    }
+
+    @Test
+    @ShouldMatchDataSet("stories.xml")
+    public void testFindStoriesFilterByGenre() {
+        List<Story> storyList = this.facade.findStories(Genre.STORY, null, null, null, null, null);
+
+        final List<Story> recentStories = recentStories().stream()
+                .filter(i -> i.getGenre().equals(Genre.STORY))
+                .limit(SIZE)
+                .collect(Collectors.toList());
+
+        assertThat(recentStories, containsStorysInOrder(storyList));
+    }
+
+    @Test
+    @ShouldMatchDataSet("stories.xml")
+    public void testFindStoriesFilterByTheme() {
+        List<Story> storyList = this.facade.findStories(null, Theme.ADVENTURE, null, null, null, null);
+
+        final List<Story> recentStories = recentStories().stream()
+                .filter(i -> i.getMainTheme().equals(Theme.ADVENTURE) ||
+                        (i.getSecondaryTheme() != null && i.getSecondaryTheme().equals(Theme.ADVENTURE)))
+                .limit(SIZE)
+                .collect(Collectors.toList());
+
+        assertThat(recentStories, containsStorysInOrder(storyList));
+    }
+
+    @Test
+    @ShouldMatchDataSet("stories.xml")
+    public void testFindStoriesFilterByInitDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2018, Calendar.DECEMBER, 31, 12, 59, 59);
+        Date initDate = calendar.getTime();
+
+        List<Story> storyList = this.facade.findStories(null, null, initDate, null, null, null);
+
+        final List<Story> recentStories = recentStories().stream()
+                .filter(i -> i.getDate().after(initDate))
+                .limit(SIZE)
+                .collect(Collectors.toList());
+
+        assertThat(recentStories, containsStorysInOrder(storyList));
+    }
+
+    @Test
+    @ShouldMatchDataSet("stories.xml")
+    public void testFindStoriesFilterByEndDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2018, Calendar.DECEMBER, 31, 12, 59, 59);
+        Date endDate = calendar.getTime();
+
+        List<Story> storyList = this.facade.findStories(null, null, null, endDate, null, null);
+
+        final List<Story> recentStories = recentStories().stream()
+                .filter(i -> i.getDate().before(endDate))
+                .limit(SIZE)
+                .collect(Collectors.toList());
+
+        assertThat(recentStories, containsStorysInOrder(storyList));
+    }
+
+    @Test
+    @ShouldMatchDataSet("stories.xml")
+    public void testFindStoriesFilterByDateBetween() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2018, Calendar.DECEMBER, 31, 12, 59, 59);
+        Date initDate = calendar.getTime();
+        calendar.set(2021, Calendar.DECEMBER, 31, 12, 59, 59);
+        Date endDate = calendar.getTime();
+
+        List<Story> storyList = this.facade.findStories(null, null, initDate, endDate, null, null);
+
+        final List<Story> recentStories = recentStories().stream()
+                .filter(i -> i.getDate().after(initDate))
+                .filter(i -> i.getDate().before(endDate))
+                .limit(SIZE)
+                .collect(Collectors.toList());
+
+        assertThat(recentStories, containsStorysInOrder(storyList));
+    }
+
+    @Test
+    @ShouldMatchDataSet("stories.xml")
+    public void testFindStoriesFilterByDateBetweenIncorrectRange() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2018, Calendar.DECEMBER, 31, 12, 59, 59);
+        Date initDate = calendar.getTime();
+        calendar.set(2015, Calendar.DECEMBER, 31, 12, 59, 59);
+        Date endDate = calendar.getTime();
+
+        List<Story> storyList = this.facade.findStories(null, null, initDate, endDate, null, null);
+
+        assertThat(storyList, is(IsEmptyCollection.empty()));
+    }
+
+
+    @Test
+    @ShouldMatchDataSet("stories.xml")
+    public void testFindStoriesFilterMiltipleFields() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2000, Calendar.DECEMBER, 31, 12, 59, 59);
+        Date initDate = calendar.getTime();
+        calendar.set(2021, Calendar.DECEMBER, 31, 12, 59, 59);
+        Date endDate = calendar.getTime();
+        List<Story> storyList = this.facade.findStories(Genre.STORY, Theme.HORROR, initDate, endDate, null, null);
+
+        final List<Story> recentStories = recentStories().stream()
+                .filter(i -> i.getGenre().equals(Genre.STORY))
+                .filter(i -> i.getMainTheme().equals(Theme.HORROR) ||
+                        (i.getSecondaryTheme() != null && i.getSecondaryTheme().equals(Theme.HORROR)))
+                .filter(i -> i.getDate().after(initDate))
+                .filter(i -> i.getDate().before(endDate))
+                .limit(SIZE)
+                .collect(Collectors.toList());
+
+        assertThat(recentStories, containsStorysInOrder(storyList));
     }
 }
