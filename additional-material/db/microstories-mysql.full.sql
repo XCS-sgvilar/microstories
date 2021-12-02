e2484400abaa531dd9eaf4758142bc052aac24d1
victorLevoso <vlfernandez@esei.uvigo.es>
2021-12-02 19:57:50 +0100

    tsk29 - fixed errors in db scripts

--- additional-material/db/microstories-mysql.full.sql
+++ additional-material/db/microstories-mysql.full.sql
@@ -20,7 +20,32 @@ FLUSH PRIVILEGES;
 create table User
 (
     login varchar(100) not null  primary key,
-    password varchar(100) not null
+    password varchar(100) not null,
+    role varchar(100) not null
+
+);
+
+create table Story
+(
+    id int auto_increment
+        primary key,
+    author varchar(100),
+    content varchar(1000) not null,
+    date datetime not null,
+    genre varchar(9) not null,
+    mainTheme varchar(15) not null,
+    publicated bit not null,
+    secondaryTheme varchar(15) null,
+    title varchar(80) not null,
+   FOREIGN KEY (author) REFERENCES User(login)
+);
+
+create table VisitDate
+(
+    storyId int not null,
+    visitDate datetime null,
+    constraint FK90v9x8f0qu1o4rBtdlsqgk3uf
+        foreign key (storyId) references Story(id)
 );
 
 create table Story
